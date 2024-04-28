bcaac40e90b641833434768aa8704e77a854e551
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-04-28 22:42:17 +0000

    added templates

--- answer.php
+++ answer.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="Number Guessing Game, in PHP" />
+  <meta name="description" content="Positive or Negative #, in PHP" />
   <meta name="keywords" content="mths, ics2o" />
   <meta name="author" content="Nathan De Silva" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -22,34 +22,40 @@
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Positive or Negative #, in PHP</span>
+        <span class="mdl-layout-title">Positive and Negative #, in PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
         <img src="./images/positive_negative.jpg" alt="positive_negative" width="250" />
       </div>
-      <h3>Select Positive or Negative.
-      </h3>
       <div class="page-content-php">
-        <form action="answer.php" method="POST">
-          <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="positive">
-            <input type="radio" id="positive" class="mdl-radio__button" name="options" value="1" checked>
-            <span class="mdl-radio__label">Positive</span>
-          </label>
-          <br />
-          <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="negative">
-            <input type="radio" id="negative" class="mdl-radio__button" name="options" value="0">
-            <span class="mdl-radio__label">Negative</span>
-          </label>
-          <br />
-          <br />
-          <!-- Accent-colored raised button with ripple -->
-          <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" type="submit">
-            FIND MOVIE TYPE
-          </button>
-        </form>
-      </div>
+        <div name="number-guessed">
+          <div class="mdl-layout__header-row">
+            <br />
+            <div class="mdl-layout__content">
+            </div>
+            <div class="mdl-layout__left">
+              <?php
+
+              $positiveButton = $_POST["options"];
+              $randomNumber = floor(rand(1, 6));
+              $randomNegativeNumber = floor(rand(1, 6)) * -1;
+
+              // process
+              if ($positiveButton == true) {
+                // output
+                echo "You selected Positive and your number is: " . $randomNumber . ".";
+                //process
+              } elseif ($positiveButton == false) {
+                // output
+                echo "You selected Negative and your number is: " . $randomNegativeNumber . ". ";
+              }
+
+              ?>
+            </div>
+          </div>
+        </div>
     </main>
   </div>
 </body>
